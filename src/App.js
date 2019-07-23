9afb62d72f6f1d6dc4be690e4b4fbf4ed573f3c8
Charles <cwang8413@yahoo.com>
2019-07-22 20:31:38 -0700

    technical issues.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -20,21 +22,20 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
+        <ProductContext.Provider value = {{products, addItem}} >
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				component = {Products}
 			/>
+        </ProductContext.Provider>
 
+        <CartContext.Provider value = {cart} >
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+        </CartContext.Provider>
 		</div>
 	);
 }
